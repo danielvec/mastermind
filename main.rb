64d71156c9623cfa49fe785bfa39c25064fa806b
DanielVec <vechioladaniel@gmail.com>
2022-03-28 20:51:33 -0700

    create a board for the game and display it upon initialization of the game

--- main.rb
+++ main.rb
@@ -1,9 +1,24 @@
 COLORS = ['red', 'green', 'blue', 'orange', 'yellow',
      'black', 'white', 'brown']
-     
+
+BOARD = [%w[_ _ _ _], %w[_ _ _ _], %w[_ _ _ _], %w[_ _ _ _],
+         %w[_ _ _ _], %w[_ _ _ _], %w[_ _ _ _], %w[_ _ _ _], 
+         %w[_ _ _ _], %w[_ _ _ _], %w[_ _ _ _], %w[_ _ _ _]]
+
+module DisplayBoard
+  def display_board
+    BOARD.each do |row|
+      p row
+    end
+  end
+end
+
 class Game
+  include DisplayBoard
+
   def initialize
     winning_colors = comp_selection
+    display_board
   end
 
   def comp_selection
